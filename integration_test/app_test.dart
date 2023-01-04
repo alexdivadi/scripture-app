8e44d2c728e881149b4c7284c9bd02b3c3b784d2
neiljaywarner <njwandroid@gmail.com>
2023-01-03 21:13:58 -0600

    Note: both of these tests fail for two different reasons, one for length of the item,one for isar issue

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,29 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:clock/clock.dart';
 
 import 'package:scripture_app/main.dart' as app;
+
+var editButton = find.byTooltip('Edit collection name');
+
+var ok = find.text('OK');
+var cancel = find.text('Cancel');
+var textField = find.byType(TextField);
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  group('end-to-end test', () {
+  group('Edit Collection Name', () {
+    // NOTE: These are probably good candidates for widget tests sooner than later.
 
-    testWidgets('Add a verse',
+    testWidgets('Edit collection name and tap OK, new name shows.',
             (tester) async {
-          app.main();
-          await tester.pumpAndSettle(Duration(seconds: 5));
-
-          expect(find.text('Scripture App', skipOffstage: true), findsOneWidget);
-          await tester.tap(find.byIcon(Icons.edit));
-          expect(find.byTooltip('Edit collection name'), findsOneWidget);
-          await tester.pumpAndSettle(Duration(seconds: 3));
-          var textfield = find.byType(TextField);
-          expect (textfield, findsOneWidget);
-          await tester.enterText(textfield, 'newName');
-          await tester.pumpAndSettle(Duration(seconds: 3));
-
+              app.main();
+              // Note: always use clock.now instead of datetime.now() esp in SUT
+              String newName = 'newName${clock.now()}';
+              await launchAndEditName(tester,newName);
+              await tester.tap(ok);
+              await tester.pumpAndSettle();
+              expect(find.text(newName), findsOneWidget);
+              await tester.pumpAndSettle(const Duration(seconds: 8));
 
             });
+
+    testWidgets('Edit collection name and tap cancel - new name not present',
+            (tester) async {
+              app.main();
+              String newName = 'newName${clock.now()}';
+              await launchAndEditName(tester, newName);
+              await tester.tap(cancel);
+              await tester.pumpAndSettle();
+              expect(find.text(newName), findsNothing);
+              await tester.pumpAndSettle(const Duration(seconds: 8));
+            });
   });
+}
+
+
+Future<void> launchAndEditName(WidgetTester tester, String newName) async {
+
+  await tester.pumpAndSettle(const Duration(seconds: 5));
+
+  expect(find.text('Scripture App', skipOffstage: true), findsOneWidget);
+  expect(editButton, findsOneWidget);
+  await tester.tap(editButton);
+  await tester.pumpAndSettle(const Duration(seconds: 2));
+  expect (textField, findsOneWidget);
+
+  await tester.enterText(textField, newName);
+  await tester.pumpAndSettle(const Duration(seconds: 2));
+
+  expect(ok, findsOneWidget);
+  expect(cancel, findsOneWidget);
+
 }
